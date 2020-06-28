1b8d1cb9af0edb12391c7c9789acef104c4e8e1d
Lezn0 <yuxinwu1234@gmail.com>
2020-06-28 11:33:41 +0800

    convert from db to auth()

--- screens/login.js
+++ screens/login.js
@@ -1,89 +1,93 @@
 // components/login.js
 
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicator } from 'react-native';
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
 import firebaseDb from '../firebaseDb';
-
+import auth from '@react-native-firebase/auth';
 
 export default class Login extends Component {
-  
   constructor() {
     super();
-    console.ignoredYellowBox = [
-      'Setting a timer'
-      ];
-    this.state = { 
-      email: '', 
+    console.ignoredYellowBox = ['Setting a timer'];
+    this.state = {
+      email: '',
       password: '',
-      isLoading: false
-    }
+      isLoading: false,
+    };
   }
 
   updateInputVal = (val, prop) => {
     const state = this.state;
     state[prop] = val;
     this.setState(state);
-  }
+  };
 
   userLogin = () => {
-    if(this.state.email === '' && this.state.password === '') {
-      Alert.alert('Enter details to signin!')
+    if (this.state.email === '' && this.state.password === '') {
+      Alert.alert('Enter details to signin!');
     } else {
       this.setState({
         isLoading: true,
-      })
-      firebaseDb
-      .auth()
-      .signInWithEmailAndPassword(this.state.email, this.state.password)
-      .then((res) => {
-        console.log(res)
-        console.log('User logged-in successfully!')
-        this.setState({
-          isLoading: false,
-          email: '', 
-          password: ''
+      });
+      auth()
+        .signInWithEmailAndPassword(this.state.email, this.state.password)
+        .then(res => {
+          console.log(res);
+          console.log('User logged-in successfully!');
+          this.setState({
+            isLoading: false,
+            email: '',
+            password: '',
+          });
+          this.props.navigation.navigate('Home');
         })
-        this.props.navigation.navigate('Home')
-      })
-      .catch(error => this.setState({ errorMessage: error.message }))
+        .catch(error => this.setState({errorMessage: error.message}));
     }
-  }
+  };
 
   render() {
-    if(this.state.isLoading){
-      return(
+    if (this.state.isLoading) {
+      return (
         <View style={styles.preloader}>
-          <ActivityIndicator size="large" color="#9E9E9E"/>
+          <ActivityIndicator size="large" color="#9E9E9E" />
         </View>
-      )
-    }    
+      );
+    }
     return (
-      <View style={styles.container}>  
+      <View style={styles.container}>
         <TextInput
           style={styles.inputStyle}
           placeholder="Email"
           value={this.state.email}
-          onChangeText={(val) => this.updateInputVal(val, 'email')}
+          onChangeText={val => this.updateInputVal(val, 'email')}
         />
         <TextInput
           style={styles.inputStyle}
           placeholder="Password"
           value={this.state.password}
-          onChangeText={(val) => this.updateInputVal(val, 'password')}
+          onChangeText={val => this.updateInputVal(val, 'password')}
           maxLength={15}
           secureTextEntry={true}
-        />   
+        />
         <Button
           color="#3740FE"
           title="Log In"
           onPress={() => this.userLogin()}
-        />   
+        />
 
-        <Text 
+        <Text
           style={styles.loginText}
           onPress={() => this.props.navigation.navigate('Signup')}>
           Don't have account? Click here to signup
-        </Text>                          
+        </Text>
       </View>
     );
   }
@@ -92,24 +96,24 @@ export default class Login extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     padding: 35,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   inputStyle: {
     width: '100%',
     marginBottom: 15,
     paddingBottom: 15,
-    alignSelf: "center",
-    borderColor: "#ccc",
-    borderBottomWidth: 1
+    alignSelf: 'center',
+    borderColor: '#ccc',
+    borderBottomWidth: 1,
   },
   loginText: {
     color: '#3740FE',
     marginTop: 25,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   preloader: {
     left: 0,
@@ -119,6 +123,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff'
-  }
+    backgroundColor: '#fff',
+  },
 });
--- screens/signup.js
+++ screens/signup.js
@@ -1,97 +1,101 @@
 import React from 'react';
-import firebaseDb from '../firebaseDb'
-import { StyleSheet, Text, View, TextInput, Button, Alert, ActivityIndicator } from 'react-native';
-
+import firebaseDb from '../firebaseDb';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  ActivityIndicator,
+} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 export default class Signup extends React.Component {
-  
   constructor() {
     super();
-    console.ignoredYellowBox = [
-      'Setting a timer'
-      ];
-    this.state = { 
+    console.ignoredYellowBox = ['Setting a timer'];
+    this.state = {
       displayName: '',
-      email: '', 
+      email: '',
       password: '',
-      isLoading: false
-    }
+      isLoading: false,
+    };
   }
 
   updateInputVal = (val, prop) => {
     const state = this.state;
     state[prop] = val;
     this.setState(state);
-  }
+  };
 
   registerUser = () => {
-    if(this.state.email === '' && this.state.password === '') {
-      Alert.alert('Enter details to signup!')
+    if (this.state.email === '' && this.state.password === '') {
+      Alert.alert('Enter details to signup!');
     } else {
       this.setState({
         isLoading: true,
-      })
-      firebaseDb
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then((res) => {
-        res.user.updateProfile({
-          displayName: this.state.displayName
+      });
+      auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then(res => {
+          res.user.updateProfile({
+            displayName: this.state.displayName,
+          });
+          console.log('User registered successfully!');
+          this.setState({
+            isLoading: false,
+            displayName: '',
+            email: '',
+            password: '',
+          });
+          this.props.navigation.navigate('Login');
         })
-        console.log('User registered successfully!')
-        this.setState({
-          isLoading: false,
-          displayName: '',
-          email: '', 
-          password: ''
-        })
-        this.props.navigation.navigate('Login')
-      })
-      .catch(error => this.setState({ errorMessage: error.message }))      
+        .catch(error => this.setState({errorMessage: error.message}));
     }
-  }
+  };
 
   render() {
-    if(this.state.isLoading){
-      return(
+    if (this.state.isLoading) {
+      return (
         <View style={styles.preloader}>
-          <ActivityIndicator size="large" color="#9E9E9E"/>
+          <ActivityIndicator size="large" color="#9E9E9E" />
         </View>
-      )
-    }    
+      );
+    }
     return (
-      <View style={styles.container}>  
+      <View style={styles.container}>
         <TextInput
           style={styles.inputStyle}
           placeholder="Name"
           value={this.state.displayName}
-          onChangeText={(val) => this.updateInputVal(val, 'displayName')}
-        />      
+          onChangeText={val => this.updateInputVal(val, 'displayName')}
+        />
         <TextInput
           style={styles.inputStyle}
           placeholder="Email"
           value={this.state.email}
-          onChangeText={(val) => this.updateInputVal(val, 'email')}
+          onChangeText={val => this.updateInputVal(val, 'email')}
         />
         <TextInput
           style={styles.inputStyle}
           placeholder="Password"
           value={this.state.password}
-          onChangeText={(val) => this.updateInputVal(val, 'password')}
+          onChangeText={val => this.updateInputVal(val, 'password')}
           maxLength={15}
           secureTextEntry={true}
-        />   
+        />
         <Button
           color="#3740FE"
           title="Signup"
           onPress={() => this.registerUser()}
         />
 
-        <Text 
+        <Text
           style={styles.loginText}
           onPress={() => this.props.navigation.navigate('Login')}>
           Already Registered? Click here to login
-        </Text>                          
+        </Text>
       </View>
     );
   }
@@ -100,24 +104,24 @@ export default class Signup extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
     padding: 35,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   inputStyle: {
     width: '100%',
     marginBottom: 15,
     paddingBottom: 15,
-    alignSelf: "center",
-    borderColor: "#ccc",
-    borderBottomWidth: 1
+    alignSelf: 'center',
+    borderColor: '#ccc',
+    borderBottomWidth: 1,
   },
   loginText: {
     color: '#3740FE',
     marginTop: 25,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   preloader: {
     left: 0,
@@ -127,6 +131,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#fff'
-  }
+    backgroundColor: '#fff',
+  },
 });
