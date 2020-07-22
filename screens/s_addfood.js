4203353646b67c3035f0c8691f6d47d220ef35ad
Chiamjiaen <64964101+Chiamjiaen@users.noreply.github.com>
2020-07-22 13:50:53 +0800

    testing commit to master

---
+++ screens/s_addfood.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import FoodForm from '../comp/FoodForm';
+
+export default class addFoodScreen extends Component {
+  state = {
+    food: {
+      name: '',
+      description: '',
+      price: '',
+    },
+  };
+
+  componentDidMount() {
+    const currentFood = this.props.route.params.food;
+
+    if (currentFood) {
+      this.setState(prevState => ({food: (prevState.food = currentFood)}));
+    }
+  }
+
+  onFoodUpdated = food => {
+    console.log(food);
+    this.props.navigation.popToTop();
+  };
+
+  render() {
+    const currentUser = this.props.route.params.user;
+    return (
+      <FoodForm
+        username={this.props.route.params.username}
+        food={this.state.food}
+        onFoodAdded={this.props.route.params.foodAddedCallback}
+        onFoodUpdated={this.onFoodUpdated}
+      />
+    );
+  }
+}
---
+++ screens/s_orders.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, FlatList, SafeAreaView} from 'react-native';
+import {ListItem, Divider} from 'react-native-elements';
+import {getOrders, toggleSellerComp} from '../comp/foodbackend';
+import auth from '@react-native-firebase/auth';
+
+export default class sOrders extends Component {
+  state = {
+    orderItems: [],
+    indexer: 0,
+    user: '',
+  };
+
+  onOrdersReceived = orderItems => {
+    this.setState(prevState => ({
+      orderItems: (prevState.orderItems = orderItems),
+    }));
+  };
+
+  componentDidMount() {
+    var newUser = auth().currentUser.displayName;
+    getOrders(newUser, this.onOrdersReceived);
+    this.setState({
+      user: newUser,
+    });
+  }
+
+  render() {
+    return this.state.orderItems.length > 0 ? (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={this.state.orderItems}
+          ItemSeparatorComponent={() => (
+            <Divider style={{backgroundColor: 'grey'}} />
+          )}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <ListItem
+                containerStyle={styles.listItem}
+                titleStyle={styles.titleStyle}
+                title={item.order}
+                subtitleStyle={styles.subtitleStyle}
+                subtitle={'Order Time: ' + item.time}
+                subtitleStyle={styles.subtitleStyle}
+                subtitle={'Customer: ' + item.useremail}
+                onPress={() => {
+                  toggleSellerComp(user, index);
+                  this.setState(prevState => ({
+                    indexer: (prevState.indexer = index),
+                  }));
+                }}
+              />
+            );
+          }}
+        />
+      </SafeAreaView>
+    ) : (
+      <View style={styles.textContainer}>
+        <Text style={styles.emptyTitle}>No Orders Found</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  listItem: {
+    marginTop: 8,
+    marginBottom: 8,
+  },
+  textContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titleStyle: {
+    fontSize: 30,
+  },
+  subtitleStyle: {
+    fontSize: 18,
+  },
+  emptyTitle: {
+    fontSize: 32,
+    marginBottom: 16,
+  },
+  emptySubtitle: {
+    fontSize: 18,
+    fontStyle: 'italic',
+  },
+});
